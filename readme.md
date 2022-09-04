f750964bb94850a9c3ef15a0b932b861384fbaa6
b3nnjoe <bennjoewho@gmail.com>
2022-09-04 16:15:15 +0200

    Examples

--- readme.md
+++ readme.md
@@ -12,6 +12,14 @@ yarn start
 Then create a Github Webhook.
 Point the webhook to a http tunnel (you can use [ngrok](https://ngrok.com)), or if you're in production simply point it to your domain.
 
+### Example:
+
+The videos currently looks like this 😆
+
+### <video src="example/Composition.mp4" width="300px" controls/>
+
+Im going to focus more on the remotion side soon
+
 ### Contribution:
 
 I appriciate all contribution! Just leave a pull request and i will hopefully approve it as soon as possible! 😄
--- server/src/utils/Render.ts
+++ server/src/utils/Render.ts
@@ -1,47 +1,47 @@
-import path from "path";
-import { bundle } from "@remotion/bundler";
-import { getCompositions, renderMedia } from "@remotion/renderer";
+import path from 'path';
+import {bundle} from '@remotion/bundler';
+import {getCompositions, renderMedia} from '@remotion/renderer';
 
 const start = async (props: any) => {
-  // The composition you want to render
-  const compositionId = "Composition";
+	// The composition you want to render
+	const compositionId = 'Composition';
 
-  // You only have to do this once, you can reuse the bundle.
-  const entry = "remotion/src/index";
-  console.log("Creating a Webpack bundle of the video");
+	// You only have to do this once, you can reuse the bundle.
+	const entry = 'remotion/src/index';
+	console.log('Creating a Webpack bundle of the video');
 
-  const bundleLocation = await bundle(path.resolve(entry), () => undefined, {
-    // If you have a Webpack override, make sure to add it here
-    webpackOverride: (config: any) => config,
-  });
+	const bundleLocation = await bundle(path.resolve(entry), () => undefined, {
+		// If you have a Webpack override, make sure to add it here
+		webpackOverride: (config: any) => config,
+	});
 
-  // Extract all the compositions you have defined in your project
-  // from the webpack bundle.
-  const comps = await getCompositions(bundleLocation, {
-    // You can pass custom input props that you can retrieve using getInputProps()
-    // in the composition list. Use this if you want to dynamically set the duration or
-    // dimensions of the video.
-    inputProps: props,
-  });
+	// Extract all the compositions you have defined in your project
+	// from the webpack bundle.
+	const comps = await getCompositions(bundleLocation, {
+		// You can pass custom input props that you can retrieve using getInputProps()
+		// in the composition list. Use this if you want to dynamically set the duration or
+		// dimensions of the video.
+		inputProps: props,
+	});
 
-  // Select the composition you want to render.
-  const composition = comps[0];
-  // Ensure the composition exists
-  if (!composition) {
-    throw new Error(`No composition with the ID ${compositionId} found.
+	// Select the composition you want to render.
+	const composition = comps[0];
+	// Ensure the composition exists
+	if (!composition) {
+		throw new Error(`No composition with the ID ${compositionId} found.
   Review "${entry}" for the correct ID.`);
-  }
+	}
 
-  const outputLocation = `out/${compositionId}.mp4`;
-  console.log("Attempting to render:", outputLocation);
-  await renderMedia({
-    composition,
-    serveUrl: bundleLocation,
-    codec: "h264",
-    outputLocation,
-    inputProps: props,
-  });
-  console.log("Render done!");
+	const outputLocation = `out/output.mp4`;
+	console.log('Attempting to render:', outputLocation);
+	await renderMedia({
+		composition,
+		serveUrl: bundleLocation,
+		codec: 'h264',
+		outputLocation,
+		inputProps: props,
+	});
+	console.log('Render done!');
 };
 
 export default start;
